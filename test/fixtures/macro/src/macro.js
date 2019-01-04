8c05051c5b28dfc95755dee8122311f877a455ea
Federico Zivolo <fzivolo@quid.com>
2019-01-04 14:29:11 +0100

    test: sample macro test

---
+++ test/fixtures/macro/src/macro.js
@@ -0,0 +1,20 @@
+const { createMacro } = require('babel-plugin-macros');
+
+function myMacro({ references, state, babel }) {
+	const { types: t } = babel;
+
+	references.default.forEach(referencePath => {
+		const parentPath = referencePath.findParent(t.isCallExpression);
+		let variableName = referencePath.findParent(t.isVariableDeclarator).node.id
+			.name;
+		if (
+			parentPath.node.arguments.length > 0 &&
+			parentPath.node.arguments[0] !== ''
+		) {
+			variableName = parentPath.node.arguments[0].value;
+		}
+		parentPath.replaceWith(t.stringLiteral(`${variableName}-macro`));
+	});
+}
+
+module.exports = createMacro(myMacro);
